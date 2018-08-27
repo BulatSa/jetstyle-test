0b9c38732432cf31fdd3a753ca79ecf92d00399a
Bulat <sadikovba@gmail.com>
2018-08-27 11:50:55 +0300

    Initial

---
+++ index.php
@@ -0,0 +1,61 @@
+﻿<?php include('header.php'); ?>
+
+<header class="header-sec">
+	<div class="header">
+		<div class="container">
+			<div class="header__top">
+				<div class="header__logo">
+					<img src="img/header/logo.png" alt="">
+				</div>
+				<div class="header__contacts">
+					<div class="header__contact">
+						<span>Екатеринбург</span>
+						<a href="tel:7 (343) 237-23-19">7 (343) 237-23-19</a>
+					</div>
+					<div class="header__contact">
+						<span>Москва</span>
+						<a href="tel:+7 (495) 789-37-36">+7 (495) 789-37-36</a>
+					</div>
+					<div class="header__contact">
+						<span>Санкт - Петербург</span>
+						<a href="tel:+7 (812) 716-4-888">+7 (812) 716-4-888</a>
+					</div>
+				</div>
+				<div class="header__license">
+					<a href="#">Продлить лицензию</a>
+				</div>
+				<div class="header__search">
+					<a href="#">
+						<img src="img/header/search.png" alt="">
+					</a>
+				</div>
+			</div>
+		</div>
+			<div class="header__menu-wrap">
+				<div class="container">
+					<nav class="header__menu">
+						<ul>
+							<li><a href="#">Решения для бизнеса</a></li>
+							<li><a href="#">Программы</a></li>
+							<li><a href="#">Демоверсии</a></li>
+							<li><a href="#">Техподдержка</a></li>
+							<li><a href="#">База знаний</a></li>
+							<li><a href="#">О компании</a></li>
+						</ul>
+					</nav>
+					<div class="header__menu-demo">
+						<a href="#" class="btn btn--trans">скачать демоверсию</a>
+					</div>
+					<div class="header__menu-cart">
+						<a href="#">
+							<span>4</span>
+							<img src="img/header/cart.png" alt="">
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+</header>
+
+
+<?php include('footer.php'); ?>
